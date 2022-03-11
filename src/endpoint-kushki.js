7fdfca94b3ed2012c98afa858d4ec1e129823d20
Easy Bug <luispalacios1996@hotmail.com>
2022-03-11 11:29:50 -0500

    Agg endpoint merchant id

--- src/endpoint-kushki.js
+++ src/endpoint-kushki.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 require("dotenv").config();
 
 const kushkiAPI = () => {
-    let privateMerchantId = "0ac2d961a9d04af99dc7e5f2cf6919ca";
+    let privateMerchantId = process.env.PRIVATE_MERCHANT_ID;
 
     return axios.create({
         baseURL: "https://api-uat.kushkipagos.com",
