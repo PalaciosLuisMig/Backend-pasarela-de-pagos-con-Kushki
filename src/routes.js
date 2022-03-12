70a84a8cf436430e4473312c9b997f896aa15032
Easy Bug <luispalacios1996@hotmail.com>
2022-03-11 20:08:47 -0500

    Actualizar en endpoint transaction-list

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.post("/api/cards", require("./api/cards"));
 router.delete("/api/void/:ticket", require("./api/void"));
-router.get("/api/transactions", require("./api/transaction-list"));
+router.get("/api/transaction-list", require("./api/transaction-list"));
 
 router.get("/", require("./api"));
 
