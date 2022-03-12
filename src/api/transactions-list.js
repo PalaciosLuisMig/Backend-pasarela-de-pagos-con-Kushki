2a54339aedc721272a14c857a4ab4d43406b3888
Easy Bug <luispalacios1996@hotmail.com>
2022-03-11 20:37:50 -0500

    Mejora en la obtención de datps del request

--- src/api/transactions-list.js
+++ src/api/transactions-list.js
@@ -2,13 +2,16 @@ const endpointKushki = require("../endpoint-kushki");
 const allowCors = require("../allowCors");
 
 const handler = async (req, res) => {
-  const from = req.query.from;
-  const to = req.query.to;
+  const from = req.body.from;
+  const to = req.body.to;
+
+  console.log("From:" + from)
+  console.log("To:" + to)
 
   return endpointKushki()
     .get(`/analytics/v1/transactions-list?from=${from}&to=${to}`)
     .then((response) => {
-      console.log(response.data);
+      console.log("Response.data:" + response.data);
       return res.send(response.data);
     })
     .catch((error) => {
